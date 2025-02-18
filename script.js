705bec32a7909f9b2afa88e2f4eb9642802d97f3
charleselm <charleselmore53@icloud.com>
2025-02-17 21:10:50 -0500

    UPDATE script.js.

--- script.js
+++ script.js
@@ -2,17 +2,36 @@
 
 // 1. Alerts, Prompts, and document.write()
 // Activity: Create an alert box that welcomes the user
+    //let alertMessage = "This is my first text in a variable";
+    //alert(alertMessage);
 // Activity: Use a prompt to ask for the user's name and display it on the page
+   // let username = prompt("What is your username?")
+   // console.log("the username is "+username);
 // Activity: Use document.write() to display a message on the page
-
+    //let favHobby = prompt("What is your favorite hobby?");
+    //document.write("My favorite hobby is "+ favHobby);
 // 2. String Concatenation
 // Activity: Concatenate two strings and log the result to the console
+    //let str1 = "Today is Monday ";
+    //let str2 = "the date is February 17th, 2025";
 
+    //console.log(str1+str2)
 // 3. Template Literals
 // Activity: Use template literals to create a sentence with variables
-
+    //let firstName = "Charles";
+    //let lastName = "Elmore";
+    //let message = `Good evening, ${firstName} ${lastName}. Have a nice night!`
+    //console.log(message);
+    //document.write(message);
 // 4. String Methods
 // Activity: Experiment with different string methods like .length, .toUpperCase(), .trim(), etc.
 
+    //let message = "  Javascript is awesome!   ";
+
+    //console.log(message.length); 
+    //console.log(message.toUpperCase())
+    //console.log(message.trim())
+    //console.log(message.includes("awesome"));
+    //console.log(message.slice(0, -5));
 // 5. Mini Project: Interactive Story
 // Activity: Ask the user for inputs and create a short story using their responses
